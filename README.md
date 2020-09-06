1261f57be618b89050b12109aeea2c3421e92b62
liushuyu6666 <liushuyu6666@users.noreply.github.com>
2020-09-05 22:56:57 -0400

    add README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+# Note
+
+## statistic
+Title | Time | memory
+-----------------|----------|-----------
+1 | 5880ms 12 | 15mb 82
+
+
+## 1. Two Sum
+### data structure
+```python
+len(nums) # calculate the length of array.
+```
--- two_sum_1.py
+++ two_sum_1.py
@@ -2,12 +2,13 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(0, len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if target == nums[i] + nums[j]:
-                    return [i, j]
+        for i in range(0, len(nums) - 1):
+            for j in range(i + 1, len(nums)):
+                if (nums[i] + nums[j] == target):
+                    return (i, j)
+        return None;
 
 if __name__ == '__main__':
-    nums = [3, 3]
+    nums = [2, 7, 11, 15]
     s = Solution();
-    print(s.twoSum(nums, 6))
+    print(s.twoSum(nums, 9))
